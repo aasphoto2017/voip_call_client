8887c531343303509969f78a26c511a9835f43f2
aasphoto2017 <aasphoto2017@gmail.com>
2017-11-14 20:44:53 -0500

    Initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name voipCallClientApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the voipCallClientApp
+ */
+angular.module('voipCallClientApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
